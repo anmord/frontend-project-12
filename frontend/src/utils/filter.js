e118413fa62e51477494230a18a54e4b70c5ac48
Anton Tarakin <anton_tarakin@mail.ru>
2026-05-20 21:47:29 +0300

    fix filter new channel3

--- frontend/src/utils/filter.js
+++ frontend/src/utils/filter.js
@@ -2,6 +2,9 @@ import leoProfanity from 'leo-profanity'
 
 leoProfanity.loadDictionary('ru')
 
-const clean = (text) => leoProfanity.clean(text).replace(/\*\*\*/g, '*****')
+const clean = (text) => {
+  const result = leoProfanity.clean(text)
+  return result.replace(/\*+/g, '*****')
+}
 
 export default { clean }
